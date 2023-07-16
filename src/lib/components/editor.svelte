28ed18d58856adceca911f11828462fcc3116194
wcrd <wcrd@pop-os.localdomain>
2023-07-16 17:30:22 -0600

    - Add basic layout

---
+++ src/lib/components/editor.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { Editor, rootCtx, defaultValueCtx } from '@milkdown/core';
+    import { commonmark } from '@milkdown/preset-commonmark';
+    import { nord } from '@milkdown/theme-nord';
+    
+    // function editor(dom) {
+    //   // to obtain the editor instance we need to store a reference of the editor.
+    //   const MakeEditor = Editor.make()
+    //     .config((ctx) => {
+    //       ctx.set(rootCtx, dom);
+    //     })
+    //     .config(nord)
+    //     .use(commonmark)
+    //     .create();
+    // }
+    // MakeEditor.then((editor) => {
+    //   // here you have access to the editor instance.
+      
+    //   // const exampleContent = "# Hello World!";
+    //   // editor.action(replaceAll(exampleContent));
+    // })
+    const markdown =
+    `# Milkdown Svelte Commonmark
+
+    > You're scared of a world where you're needed.
+
+    This is a demo for using Milkdown with **Svelte**.`
+
+    function editor(dom) {
+        Editor.make()
+            .config((ctx) => {
+            ctx.set(rootCtx, dom)
+            ctx.set(defaultValueCtx, markdown)
+            })
+            .config(nord)
+            .use(commonmark)
+            .create();
+    }
+</script>
+    
+<div use:editor />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,27 @@
 <script>
-    import { Editor, rootCtx, defaultValueCtx } from '@milkdown/core';
-    import { commonmark } from '@milkdown/preset-commonmark';
-    import { nord } from '@milkdown/theme-nord';
-    
-    // function editor(dom) {
-    //   // to obtain the editor instance we need to store a reference of the editor.
-    //   const MakeEditor = Editor.make()
-    //     .config((ctx) => {
-    //       ctx.set(rootCtx, dom);
-    //     })
-    //     .config(nord)
-    //     .use(commonmark)
-    //     .create();
-    // }
-    // MakeEditor.then((editor) => {
-    //   // here you have access to the editor instance.
-      
-    //   // const exampleContent = "# Hello World!";
-    //   // editor.action(replaceAll(exampleContent));
-    // })
-    const markdown =
-    `# Milkdown Svelte Commonmark
-
-    > You're scared of a world where you're needed.
-
-    This is a demo for using Milkdown with **Svelte**.`
-
-    function editor(dom) {
-        Editor.make()
-            .config((ctx) => {
-            ctx.set(rootCtx, dom)
-            ctx.set(defaultValueCtx, markdown)
-            })
-            .config(nord)
-            .use(commonmark)
-            .create();
-    }
+    import Editor from "$lib/components/editor.svelte";
 </script>
+
+<div class="flex flex-col h-screen">
+    <div id="header" class="flex flex-row h-8 bg-green-200">
+        <p>Header controls</p>
+    </div>
+    <div id="editor-container" class="flex flex-row h-100 flex-grow">
+        <div class="w-1/6 flex border border-black">
+            <p>Menu</p>
+        </div>
     
-<div use:editor />
+        <div class="w-5/6 flex flex-row ">
+            <div class="w-3/6">
+                <Editor></Editor>
+            </div>
+        
+            <div class="w-3/6 border">
+                Toggle Area (to be moved into Editor / super component and have shared state)
+            </div>
+        </div>
+    </div>
+    <div id="footer" class="flex flex-row h-6 bg-blue-600 text-white">
+        <p>Footer</p>
+    </div>
+</div>
